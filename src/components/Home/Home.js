2cbd0a25a81a955c61f79e96e8b63c0fd8e7d691
Pierce Woods <pierce.woods95@gmail.com>
2021-09-06 18:03:14 +0100

    18-19 - Error handling

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,20 +5,33 @@ import BlogList from '../BlogList/BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   // cannot use async/await here, can externalise function to do so
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then(res => {
+        if(!res.ok) {
+          throw Error("Couldn't fetch data");
+        }
         return res.json();
       })
       .then(data => {
         setBlogs(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch(err => {
+        setIsLoading(false);
+        setError(err.message);
       });
   }, []);
 
   return (
     <div className={styles.content}>
+      {error && <p>{error}</p>}
+      {isLoading && <p>Loading...</p>}
       {blogs && <BlogList blogs={blogs} title="Showing all blogs" />}
     </div>
   )
