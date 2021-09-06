46c60fca2f17894cf9294e58f1e0b2848dc281a2
Pierce Woods <pierce.woods95@gmail.com>
2021-09-06 15:55:44 +0100

    10-13 - useStates and Shared components

---
+++ src/components/BlogList/BlogList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './BlogList.module.scss';
+
+const BlogList = ({blogs, title, handleDelete}) => {
+  return (
+    <div className={styles.BlogList}>
+      <h2>{title}</h2>
+      {blogs.map((blog) => (
+        <article className={styles.blog} key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by: {blog.author}</p>
+          <button className="btn__tertiary" onClick={() => handleDelete(blog.id)}>Delete</button>
+        </article>
+      ))}
+    </div>
+  )
+};
+
+export default BlogList;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import styles from './Home.module.scss';
 import { useState } from 'react';
+import BlogList from '../BlogList/BlogList';
 
 const Home = () => {
-  const [name, setName] = useState('Mario');
-  const [age, setAge] = useState(25);
-  
-  const handleClick = () => {
-    setName('Luigi');
-    setAge(30);
+  const [blogs, setBlogs] = useState(
+    [
+      { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+      { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+      { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]
+  );
+
+  const handleDelete = id => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
   }
 
   return (
     <div className={styles.content}>
-      <h1>Dojo Dingo</h1>
-      <p>{name} is {age} years old</p>
-      <button className="btn__primary" onClick={handleClick}>Click me</button>
+      <BlogList blogs={blogs} title="Showing all blogs" handleDelete={handleDelete} />
     </div>
   )
 
